a12e4d194513ddbf67718502bbd0ca6b40f42272
gene-radcliffe <g_radcliffe82@hotmail.com>
2018-08-06 16:04:33 -0400

    Completed all 10 exercises.

--- homework.js
+++ homework.js
@@ -34,7 +34,7 @@ function fahrenheitToCelsius(temperature){
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
 function celsiusToFahrenheit(temperature){
-    return()
+    return(temperature * (9/5)+32);
 }
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -42,11 +42,24 @@ function celsiusToFahrenheit(temperature){
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
+function fahrenheitToKelvin(temperature){
+    temperature = fahrenheitToCelsius(temperature);
 
+    if (temperature ==0){
+        return 273.15;
+    }     
+    return temperature + 273.15;
+}
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
-
+function lesser(num1, num2){
+    if(num1<num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
 // in the specified language. The supported languages and their
@@ -58,9 +71,42 @@ function celsiusToFahrenheit(temperature){
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
+function multigreeting(name, code){
 
+    switch (code){
+       case "en":
+            return "Hello, " + name + "!"
+        case "es":
+            return "¡Hola, " + name + "!"
+        case "fr":
+            return "Bonjour, " + name + "!"
+        case "eo":
+            return "Saluton, " + name + "!"
+        default: 
+            return ;
+    }
+}
 // 10. Create a function, howOld, that takes a birthdate and a another
 // date (the "current date") and returns how many years old the person
 // with that birthdate was on that date. The dates should be JavaScript
 // Date objects.
 // If the birth date is after than the current date, return nothing.
+function howOld(birthdate, date){
+    var difference = date - birthdate // will return milliseconds
+    var convertToYear = difference / 31104000000; // 31104000000 is the number of milliseconds in a year
+    var returnVal;
+     
+    
+    var truncate = Math.floor(convertToYear);   
+    returnVal = truncate ; //default return for newborns
+
+    //check if it is before your birthday
+    var date1 = date.getMonth() + date.getDay()
+    var date2 = birthdate.getMonth() + birthdate.getDay()
+    if(date1 < date2){
+        return returnVal -1;
+        
+    }
+    
+    return returnVal;
+}
